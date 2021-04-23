2c73452b6d25b7fb8fa0c0ed74afd40c3a534fef
rohan-empire-k <rohan.bhatt@bacancy.com>
2021-04-23 15:57:03 +0530

    Done 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [likes,setLikes] = useState(100);
+  const [liked,setLiked] = useState(false);
+  const [disliked,setDisliked] = useState(false);
+  const [disLikes,setDisLikes] = useState(25);
+
+  const increaseLikes = () =>
+  {
+    if(liked)
+    {
+      setLikes(likes-1);
+      setLiked(false);
+    }
+    else
+    {
+      setLikes(likes+1);
+      setLiked(true);
+      if(disliked)
+      {
+        setDisliked(false);
+        setDisLikes(disLikes-1);
+      }
+      
+    }
+  }
+
+  const increaseDisLikes = () =>
+  {
+    if(disliked)
+    {
+      setDisLikes(disLikes-1);
+      setDisliked(false);
+    }
+    else
+    {
+      setDisLikes(disLikes+1);
+      setDisliked(true);
+      if(liked)
+      {
+        setLikes(likes-1);
+        setLiked(false);
+      }
+      
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Live task</h1>
+        <div>
+            <span>{`Number of likes ${likes}`}</span>
+        </div>
+        <div>
+            <span>{`Number of dislikes ${disLikes}`}</span>
+        </div>
+        <div>
+          <button className={liked ? "like-button liked" : "like-button"} onClick={increaseLikes}>Like | {likes}</button>
+          <button className={disliked ? "dislike-button disliked" : "dislike-button"} onClick={increaseDisLikes}>Dislike | {disLikes}</button>
+        </div>
     </div>
   );
 }
